01aac57f5b0fcf9e7a1a4c466fb00bc38c396fbe
Jamilton Alonso Quintero Osorio <transportador1118@gmail.com>
2022-04-17 17:02:06 -0500

    JQ add readme instruccions and enviroments-variables

---
+++ env-variables.bat
@@ -0,0 +1,4 @@
+setx DATASOURCE_URL_MPOS "jdbc:sqlserver://localhost;databaseName=MposPrueba"
+setx DATASOURCE_USERNAME_MPOS "jamilton"
+setx DATASOURCE_PASSWORD_MPOS "Jd.5162805"
+set /p=Hit ENTER to continue...
